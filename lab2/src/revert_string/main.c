56ee92a9424191df9bc0b9ff42b62e05542906c0
DeadDoom1 <fgurbanski@mail.ru>
2024-10-05 19:38:57 +0300

    Done

--- lab2/src/revert_string/main.c
+++ lab2/src/revert_string/main.c
@@ -1,17 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <dlfcn.h>
 
 #include "revert_string.h"
 
+void* getFunctionPointer(void* lib, const char* funcName) {
+	void* fptr = dlsym(lib, funcName);
+	if (!fptr) {
+		fprintf(stderr, "Could not get function pointer for %s\n  error is: %s\n\n", funcName, dlerror());
+		return NULL;
+	}
+	return fptr;
+}
+
 int main(int argc, char *argv[])
 {
+/*
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wunused-but-set-variable"
+	void (*fptr_null)(int);
+#pragma GCC diagnostic pop
+	void (*fptr_RevertString)(char*);
+	
+
+	void* tq84_lib = dlopen("libtq84.a", RTLD_LAZY | RTLD_GLOBAL);
+	if (!tq84_lib) {
+		fprintf(stderr, "Could not open libtq84.so\n");
+		exit(1);
+	}
+
+	fptr_RevertString = getFunctionPointer(tq84_lib, "RevertString");
+	fptr_null = getFunctionPointer(tq84_lib, "doesNotExist");
 	if (argc != 2)
 	{
 		printf("Usage: %s string_to_revert\n", argv[0]);
 		return -1;
 	}
-
+	*/
 	char *reverted_str = malloc(sizeof(char) * (strlen(argv[1]) + 1));
 	strcpy(reverted_str, argv[1]);
 
--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -1,7 +1,12 @@
 #include "revert_string.h"
-
+#include <string.h>
 void RevertString(char *str)
 {
-	// your code here
+	char buf;
+	for (int i = 0; i < strlen(str)/2; i++) {
+		buf=str[i];
+		str[i]=str[strlen(str)-i-1];
+		str[strlen(str) - i-1]=buf;
+	}
 }
 
