e5fee111d309b7ee8ac6553749891576ada3dde8
DeadDoom1 <fgurbanski@mail.ru>
2024-12-21 07:49:57 +0000

    добавлен find_min_max.c, makefile, parallel_min_max.c.

---
+++ lab4/src/num5/sum_lib.h
@@ -0,0 +1,13 @@
+#ifndef SUM_LIB_H
+#define SUM_LIB_H
+
+struct SumArgs {
+    int *array;
+    int begin;
+    int end;
+};
+
+int Sum(const struct SumArgs *args);
+
+#endif // SUM_LIB_H
+
---
+++ lab4/src/zombie.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+int main() {
+    pid_t pid;
+
+    // Создаем дочерний процесс
+    pid = fork();
+
+    if (pid < 0) {
+        // Ошибка при создании процесса
+        perror("fork failed");
+        exit(1);
+    } else if (pid == 0) {
+        // Дочерний процесс
+        printf("Child process (PID: %d) is running...\n", getpid());
+        sleep(5);
+        printf("Child process (PID: %d) is exiting...\n", getpid());
+        exit(0);
+    } else {
+        // Родительский процесс
+        printf("Parent process (PID: %d) is running...\n", getpid());
+        sleep(10);
+        printf("Parent process (PID: %d) is exiting...\n", getpid());
+
+        // Родительский процесс вызывает wait для получения статуса завершения дочернего процесса
+       //wait(NULL);
+    }
+
+    return 0;
+}
